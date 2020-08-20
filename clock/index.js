4c628a2f6b80f9223a2966df7e55d73ac320b63c
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2020-08-20 14:59:30 +0200

    Init of JS 30 folder

---
+++ clock/index.js
@@ -0,0 +1,32 @@
+const hand = document.querySelector('.hand')
+const hourHand = document.querySelector('.hour-hand')
+const minuteHand = document.querySelector('.min-hand')
+const secondHand = document.querySelector('.second-hand')
+
+const setHour = () => {
+	const now = new Date()
+	const hour = now.getHours()
+	const hourDegrees = (hour / 12) * 360 + 90
+	hourHand.style.transform = `rotate(${hourDegrees}deg)`
+}
+
+const setMinute = () => {
+	const now = new Date()
+	const minute = now.getMinutes()
+	const minuteDegrees = (minute / 60) * 360 + 90
+	minuteHand.style.transform = `rotate(${minuteDegrees}deg)`
+}
+
+const setSeconds = () => {
+	const now = new Date()
+	const seconds = now.getSeconds()
+	const secondDegrees = (seconds / 60) * 360 + 90
+	console.log(secondDegrees)
+	secondHand.style.transform = `rotate(${secondDegrees}deg)`
+}
+
+setInterval(() => {
+	setHour()
+	setMinute()
+	setSeconds()
+}, 1000)
