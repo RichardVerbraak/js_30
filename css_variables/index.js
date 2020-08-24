b794f945101fb82b703f2827d0488213fa9eba84
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2020-08-24 15:29:26 +0200

    Added in the JS for changing CSS variables

---
+++ css_variables/index.js
@@ -0,0 +1,29 @@
+const inputs = document.querySelectorAll('.controls input')
+const root = document.documentElement
+
+// If I used an arrow function, 'this' would be bound to the scope of this function, so it would be referencing nothing hence undefined returning
+// With a regular function it depends on where the function would be called
+// function handleUpdate() {
+// 	const suffix = this.dataset.sizing || ''
+// 	root.style.setProperty(`--${this.name}`, this.value + suffix)
+// }
+
+// inputs.forEach((input) => input.addEventListener('change', handleUpdate))
+// inputs.forEach((input) => input.addEventListener('mousemove', handleUpdate))
+
+const handleUpdate = (e) => {
+	const suffix = e.target.dataset.sizing || ''
+	root.style.setProperty(`--${e.target.name}`, e.target.value + suffix)
+}
+
+inputs.forEach((input) => {
+	input.addEventListener('change', (e) => {
+		handleUpdate(e)
+	})
+})
+
+inputs.forEach((input) => {
+	input.addEventListener('mousemove', (e) => {
+		handleUpdate(e)
+	})
+})
