02a9d4589c45fc4ba143afb8757c7cd4c2a5d77f
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2020-08-24 15:29:38 +0200

    Added additional comments

--- css_variables/index.js
+++ css_variables/index.js
@@ -1,6 +1,7 @@
 const inputs = document.querySelectorAll('.controls input')
 const root = document.documentElement
 
+/////////// Wes Bos's Version
 // If I used an arrow function, 'this' would be bound to the scope of this function, so it would be referencing nothing hence undefined returning
 // With a regular function it depends on where the function would be called
 // function handleUpdate() {
@@ -11,6 +12,7 @@ const root = document.documentElement
 // inputs.forEach((input) => input.addEventListener('change', handleUpdate))
 // inputs.forEach((input) => input.addEventListener('mousemove', handleUpdate))
 
+// My solution
 const handleUpdate = (e) => {
 	const suffix = e.target.dataset.sizing || ''
 	root.style.setProperty(`--${e.target.name}`, e.target.value + suffix)
